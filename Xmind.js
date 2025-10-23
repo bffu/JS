a686d5af7def52efe5e1ae1f17822f1adb7fb82b
bffu <133213947+bffu@users.noreply.github.com>
2025-10-23 11:42:21 +0800

    创建 Xmind.js

---
+++ Xmind.js
@@ -0,0 +1,27 @@
+[rewrite_local]
+
+# 拦截 Xmind 的 App Store 激活地址
+^http[s]?:\/\/www.xmind.app\/_api\/appstore\/active url script-response-body xmind_pro.js
+
+[mitm] 
+
+# 启用对新域名的 MITM
+hostname = www.xmind.app
+
+var body = $response.body.replace(/.+/g, '{
+  "status": "ok",
+  "deviceId": "09DA9FCA-A929-402A-B39D-F477E1F8D9A9",
+  "bindXmind": 1,
+  "subscriptionStatus": "ACTIVATED",
+  "_code": 200,
+  "originalTransactionID": "FAKE_PRO_ID",
+  "expireTime": 4070088000000, // 2099 年的时间戳
+  "receiptId": "7886bb3d279ad652837ab71e28231743",
+  "requestId": "F8E4BDBC-28F6-4E01-9574-E86AC2C79146",
+  "is_pro": true 
+}');
+
+// 3. 脚本执行完成：
+$done({'body': body});
+
+
