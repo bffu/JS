4d17b9e596507a81c99c0db6b10e4dcbd0558ad0
bffu <133213947+bffu@users.noreply.github.com>
2026-08-07 00:28:25 +0800

    Update Calflow.js

--- Calflow.js
+++ Calflow.js
@@ -1,6 +1,6 @@
 
 [rewrite_local]
-^https:\/\/api\.rc-backup\.com\/v1\/subscribers\/ url script-response-body https://raw.githubusercontent.com/bfffu/Js/main/Calflow.js 
+^https:\/\/api\.rc-backup\.com\/v1\/subscribers\/ url script-response-body https://raw.githubusercontent.com/bffu/Js/main/Calflow.js 
 
 [mitm]
 hostname = api.rc-backup.com
